3e8634ece0faedb8b776426efb1f7b80594503b5
Test P02 <testp02@student-p-02.local>
2016-09-16 14:04:04 +0530

    Initial Commit

---
+++ JCSliderControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCSliderControl
+//
+//  Created by Test P02 on 16/09/16.
+//  Copyright © 2016 Test P01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JCSliderControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JCSliderControl
+//
+//  Created by Test P02 on 16/09/16.
+//  Copyright © 2016 Test P01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
